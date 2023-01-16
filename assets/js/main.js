d95ab301f24bbbe259c94c5c7cca5933de8915c0
Robert Weis <gbobweis@gmail.com>
2023-01-16 19:59:38 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
