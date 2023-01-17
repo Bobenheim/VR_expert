6dfcf3a9c0d64385d85717a88c9b1fe4586e6904
Robert Weis <gbobweis@gmail.com>
2023-01-17 20:52:56 +0100

    reworked content. gallery left

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,46 @@ function myFunction() {
     }
   }
 
+/* COLLAPSIBLE FUNCTION */
   const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+/* BEFORE AFTER SLIDER */
+function beforeAfter() {
+  document.getElementById('compare').style.width = document.getElementById('slider').value + "%";
+}
+
+/* SHOW HIDE CONTENT */
+function toggleDiv(divid)
+  {
+ 
+    varon = divid + 'on';
+    varoff = divid + 'off';
+ 
+    if(document.getElementById(varon).style.display == 'block')
+    {
+    document.getElementById(varon).style.display = 'none';
+    document.getElementById(varoff).style.display = 'block';
+    }
+   
+    else
+    {  
+    document.getElementById(varoff).style.display = 'none';
+    document.getElementById(varon).style.display = 'block'
+    }
+} 
+
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
