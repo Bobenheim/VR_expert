c1b0a8f53ef83ec3ce198bb6918b05c50302bca6
Robert Weis <gbobweis@gmail.com>
2023-01-19 10:26:01 +0100

    edited visibility of before after slider on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+
 
 /* COLLAPSIBLE FUNCTION */
   const collapsibles = document.querySelectorAll(".collapsible");
@@ -21,36 +13,7 @@ function beforeAfter() {
   document.getElementById('compare').style.width = document.getElementById('slider').value + "%";
 }
 
-/* SHOW HIDE CONTENT */
-function toggleDiv(divid)
-  {
- 
-    varon = divid + 'on';
-    varoff = divid + 'off';
- 
-    if(document.getElementById(varon).style.display == 'block')
-    {
-    document.getElementById(varon).style.display = 'none';
-    document.getElementById(varoff).style.display = 'block';
-    }
-   
-    else
-    {  
-    document.getElementById(varoff).style.display = 'none';
-    document.getElementById(varon).style.display = 'block'
-    }
-} 
 
-
-
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
 
 /* GALLERY */
 let slideIndex = 1;
